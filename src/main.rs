9009a57258a0c92716d92db283dfa78156567998
James Pogue <jamesipogue@protonmail.com>
2020-09-24 06:00:25 -0700

    add new arguments for resolution, framerate, and scaling

--- src/main.rs
+++ src/main.rs
@@ -93,15 +93,15 @@ impl GollyBuff {
         neighbors
     }
 
-    fn classic_life(&mut self, survive_rule: &Vec<u8>, born_rule: &Vec<u8>) {
+    fn classic_life(&mut self, survive_rule: &[bool; 9], born_rule: &[bool; 9]) {
         let neighborvec = self.all_live_neighbors();
         for (value, neighbors) in self.pixels.iter_mut().zip(neighborvec) {
             if *value == 0x000000 {
-                if born_rule.contains(&neighbors) {
+                if born_rule[neighbors as usize] {
                     *value = ALIVE;
                 }
             } else {
-                if !survive_rule.contains(&neighbors) {
+                if !survive_rule[neighbors as usize] {
                     *value = 0x000000;
                 }
             }
@@ -131,62 +131,117 @@ impl GollyBuff {
             self.pixels[cell] = ALIVE;
         }
     }
-
     fn clear(&mut self) {
         self.pixels = vec![0x000000; self.pixels.len()];
     }
 }
 
-fn parse_rule(rule: &str) -> (Vec<u8>, Vec<u8>) {
-    let mut survive: Vec<u8> = Vec::new();
-    let mut born: Vec<u8> = Vec::new();
+fn parse_rule(rule: &str) -> ([bool; 9], [bool; 9]) {
+    let mut survive = [false; 9];
+    let mut born = [false; 9];
 
     let mut rule = rule.chars();
     for num in rule.by_ref().take_while(|x| *x != '/') {
-        survive.push(num.to_digit(10).unwrap() as u8);
+        let num = num.to_digit(10).expect("Invalid rule") as usize;
+        assert!(num < survive.len(), "Only numbers 0-8 are permitted. Something cannot have 9 neighbors");
+        survive[num] = true;
     }
     for num in rule {
-        born.push(num.to_digit(10).unwrap() as u8);
+        let num = num.to_digit(10).expect("Invalid rule") as usize;
+        assert!(num < born.len(), "Only numbers 0-8 are permitted. Something cannot have 9 neighbors");
+        born[num] = true;
     }
     (survive, born)
 }
 
 fn main() {
-    let (width, height) = (350, 200);
-    let mut buff = GollyBuff::new("yuh", width, height, minifb::Scale::X4);
-
-    let day_night_survive: Vec<u8> = vec![2, 3, 5];
-    let day_night_born: Vec<u8> = vec![3, 5];
-
     let matches = App::new("Ian's game of life")
-        .args_from_usage("-r --rule=[RULE] 'Sets the rule to be used. Default is 23/3'")
+        .args_from_usage(
+            "-r --rule=[RULE]       'Sets the rule to be used. Default is 23/3'
+            -f --framerate=[FPS]    'Sets the framerate. Default is 60'
+            -d --dimensions=[RES]   'Sets the resolution. Default is 350x200'
+            -s --scaling=[SCALE]    'Sets the display scaling. Can be powers of 2 up to 32'")
+        .name("Ian's game of life")
+        .author("Author: me, duh")
         .get_matches();
 
     let rule = match matches.value_of("rule") {
         Some(string) => parse_rule(string),
-        None => (vec![2, 3], vec![3]),
+        None => {
+            let mut survive = [false; 9];
+            let mut born = [false; 9];
+            survive[2] = true;
+            survive[3] = true;
+            born[3] = true;
+            (survive, born)
+        }
     };
 
+    let framerate = match matches.value_of("framerate") {
+        Some(fps) => fps.to_string().parse().unwrap_or(60),
+        None => 60,
+    };
+
+    let scale = match matches.value_of("scaling") {
+        Some("1") => minifb::Scale::X1,
+        Some("2") => minifb::Scale::X2,
+        Some("4") => minifb::Scale::X4,
+        Some("8") => minifb::Scale::X8,
+        Some("16") => minifb::Scale::X16,
+        Some("32") => minifb::Scale::X32,
+        None => minifb::Scale::FitScreen,
+        _ => panic!("Invalid scale. Can be 1, 2, 4, 8, 16, or 32"),
+    };
+
+    let (width, height): (usize, usize) = match matches.value_of("dimensions") {
+        Some(res) => {
+            let mut iter = res.split('x');
+            (iter.next().unwrap_or("invalid dimensions").parse().unwrap(), iter.next().unwrap_or("invalid dimensions").parse().unwrap())
+        }
+        None => (300,200),
+    };
+
+    let mut buff = GollyBuff::new("yuh", width, height, scale);
+
     /*
-    for i in buff.neighbors(1300).iter() {
-        buff.pixels[*i] = WHITE;
-    }
+       for i in buff.neighbors(1300).iter() {
+       buff.pixels[*i] = WHITE;
+       }
 
-    let glider = 15100;
+       let glider = 15100;
 
-    for i in 0..=2 {
-        buff.pixels[glider + i] = WHITE;
-    }
+       for i in 0..=2 {
+       buff.pixels[glider + i] = WHITE;
+       }
 
-    buff.pixels[glider + buff.width + 2] = WHITE;
-    buff.pixels[glider + (buff.width * 2) + 1] = WHITE;
-    buff.pixels[glider + (buff.width * 2) + 2] = WHITE;
+       buff.pixels[glider + buff.width + 2] = WHITE;
+       buff.pixels[glider + (buff.width * 2) + 1] = WHITE;
+       buff.pixels[glider + (buff.width * 2) + 2] = WHITE;
 
-    buff.update();
-    */
+       buff.update();
+       */
+
+
+    let anchor = 10000;
+
+    let mut pix = buff.move_index(anchor, 1, -1);
+    buff.pixels[pix] = ALIVE;
+    println!("{}", buff.elementary_parents(anchor));
+
+    /*
+       for x in 0..20 {
+       for y in 0..buff.height as i32 {
+       let pix = buff.move_index(anchor, x, y);
+       buff.pixels[pix] = ALIVE;
+       }
+       }
+       */
+
+    let skip = framerate / 60;
+    let mut ctr = 0;
 
     buff.win
-        .limit_update_rate(Some(time::Duration::from_millis(30)));
+        .limit_update_rate(Some(time::Duration::from_secs(1).div_f32(60 as f32)));
     let mut paused = true;
     let mode = |buff: &mut GollyBuff| buff.classic_life(&rule.0, &rule.1);
     loop {
@@ -196,8 +251,14 @@ fn main() {
                 buff.pixels[pos] = ALIVE;
             }
         }
-        if buff.win.is_key_released(minifb::Key::Space) {
+        if buff.win.is_key_pressed(minifb::Key::Space, minifb::KeyRepeat::No) {
             paused = !paused;
+            if paused {
+                buff.win.limit_update_rate(Some(time::Duration::from_secs(1).div_f32(60 as f32)));
+            } else {
+                buff.win
+                    .limit_update_rate(Some(time::Duration::from_secs(1).div_f32(framerate as f32)));
+            }
         }
         if !paused {
             //buff.classic_life(&day_night_survive, &day_night_born);
@@ -205,23 +266,28 @@ fn main() {
         }
         if buff
             .win
-            .is_key_pressed(minifb::Key::R, minifb::KeyRepeat::No)
-        {
-            buff.clear();
-        }
+                .is_key_pressed(minifb::Key::R, minifb::KeyRepeat::No)
+                {
+                    buff.clear();
+                }
         if buff
             .win
-            .is_key_pressed(minifb::Key::Escape, minifb::KeyRepeat::No)
-        {
-            break;
-        }
+                .is_key_pressed(minifb::Key::Escape, minifb::KeyRepeat::No)
+                {
+                    break;
+                }
         if paused
             && buff
                 .win
                 .is_key_pressed(minifb::Key::Period, minifb::KeyRepeat::Yes)
-        {
-            mode(&mut buff);
+                {
+                    mode(&mut buff);
+                }
+
+        ctr += 1;
+        ctr %= skip;
+        if ctr == 0 {
+            buff.update();
         }
-        buff.update();
     }
 }
